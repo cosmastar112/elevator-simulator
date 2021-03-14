314aea3107308e90f830b120b76a512282af5c3d
cosmastar112 <cosmastar112@gmail.com>
2021-03-14 15:19:07 +0400

    отправка и сериализация формы

--- src/js/root.system.js
+++ src/js/root.system.js
@@ -2,7 +2,46 @@
 
     function init()
     {
-        console.log('init system');
+        let btn = document.getElementById('system_params_form__submit_btn');
+        if (!btn) {
+            return;
+        }
+        btn.addEventListener('click', _startHandler);
+    }
+
+    function _startHandler(event)
+    {
+        event.preventDefault();
+        // отключить кнопку
+        event.target.disabled = true;
+        // сериализовать форму
+        let serializedForm = _serializeForm();
+        console.log(serializedForm);
+
+    }
+
+    function _serializeForm()
+    {
+        let form = document.getElementById('system_params_form');
+        if (!form) {
+            return null;
+        }
+
+        let total_floors = _getSelectedOptionValue(form.elements, 0);
+        let total_elevators = _getSelectedOptionValue(form.elements, 1);
+        let lifting_power = _getSelectedOptionValue(form.elements, 2);
+
+        return {
+            'total_floors': total_floors,
+            'total_elevators': total_elevators,
+            'lifting_power': lifting_power,
+        };
+    }
+
+    function _getSelectedOptionValue(selectedElements, index)
+    {
+        let select = selectedElements[index];
+        return select.options[select.selectedIndex].value;
     }
 
     root.registerModule({
